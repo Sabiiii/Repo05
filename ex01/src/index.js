c80e4e05aa80427fc3364c2bc0b2a9a7437ecc68
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 12:36:09 +0400

    Adding escaping literal quotes in strings;

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var myFirstString = "I think therefore I \"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren't."; 
+
+    return {
+
+        myFirstString,
+        mySecondString
+    };
+
+}
+
+console.log(main());
+
+module.exports = main;
