475b7f8d2e27383271eed012196c8497c7b9442d
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 14:42:14 +0400

    Adding constructing strings with variables

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+
+    var myName = "Sabina";
+    var myString = "My name is " + myName + " and I am well!";
+
+
+
+    return myString;
+
+}
+
+console.log(main());
+
+module.exports = main;
