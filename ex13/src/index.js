054d8c77b0d285ea3e385883cb0772e3e554a5e8
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 15:34:51 +0400

    Adding bracket notation to find nth to last character

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+    lastName = "Bezos";
+    var thirdToLastLetterOfLastName = lastName[lastName.length - 3]
+
+
+    return thirdToLastLetterOfLastName;
+
+}
+
+console.log(main());
+
+module.exports = main;
