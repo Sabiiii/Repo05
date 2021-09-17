d48d3acf323feadb5b78619804d38a85102c16d3
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 22:04:03 +0400

    Adding Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb;
+
+
+
+    return wordBlanks;
+
+}
+
+console.log(main());
+
+module.exports = main;
