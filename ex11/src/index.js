4e1ccb9b7903d68afd83e52da05d6ab92d3ed175
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 15:22:00 +0400

    Adding bracket notation to find nth character

---
+++ ex11/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+    lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2]
+
+
+    return thirdLetterOfLastName;
+
+}
+
+console.log(main());
+
+module.exports = main;
